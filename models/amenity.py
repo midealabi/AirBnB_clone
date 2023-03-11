af4b3f3ad6113cb12e6a75d9baab9ee7bbecde7d
midealabi <ayoomide@yahoo.com>
2023-03-11 10:06:29 +0000

    ayo

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseMod
+
+
+class Amenity(BaseModel):
+    """Represent an amenity.
+
+    Attributes:
+         name (str): The name of the amenity.
+    """
+
+   name =""
